92c492030be0a2c2ce4b8a543d5570ce17792704
Harshita Jinnala <119839384+Harshitajinnala@users.noreply.github.com>
2022-12-09 20:38:55 +0530

    #include<stdio.h> #include<math.h> int main() {     float a;     scanf("%f",&a);     float s,p;     s=(3.14)*a*a;     p=2*(3.14)*a;     printf("%0.2f %0.2f",s,p);
    }

---
+++ Area_and_Perimeter_of_Circle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a;
+    scanf("%f",&a);
+    float s,p;
+    s=(3.14)*a*a;
+    p=2*(3.14)*a;
+    printf("%0.2f
+%0.2f",s,p);
+    
+}
