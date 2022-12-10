55eaa822dced1c0cf591ac47117255f5c644fa5c
Harshita Jinnala <119839384+Harshitajinnala@users.noreply.github.com>
2022-12-10 21:34:49 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n%2!=0)     {         printf("weird");     }     else if(n>5,n%2==0)     {         printf("not weird");     }     else if(n<6,n>20,n%2==0)     {         printf("weird");     }     else     {         printf("not weird");     } }

---
+++ Weird_or_not_Weird.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2!=0)
+    {
+        printf("weird");
+    }
+    else if(n>5,n%2==0)
+    {
+        printf("not weird");
+    }
+    else if(n<6,n>20,n%2==0)
+    {
+        printf("weird");
+    }
+    else
+    {
+        printf("not weird");
+    }
+}
