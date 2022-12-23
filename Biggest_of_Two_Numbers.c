8534c88eb39b5d41f3e0c241ffc26b73b2b47baa
Harshita Jinnala <119839384+Harshitajinnala@users.noreply.github.com>
2022-12-23 21:37:21 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     if (a>b)     {         printf("%d",a);     }     else if (b>a)     {         printf("%d",b);     } }

--- Biggest_of_Two_Numbers.c
+++ Biggest_of_Two_Numbers.c
@@ -3,12 +3,12 @@ int main()
 {
     int a,b;
     scanf("%d%d",&a,&b);
-    if(a>b)
+    if (a>b)
     {
         printf("%d",a);
     }
-    else
+    else if (b>a)
     {
         printf("%d",b);
     }
-}
+}   
