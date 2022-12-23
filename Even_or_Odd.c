3d3ff8fac400eecad3db0e5b0cb0fcb076d8f4db
Harshita Jinnala <119839384+Harshitajinnala@users.noreply.github.com>
2022-12-23 21:38:16 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     if (a%2!=0)     {         printf("Odd");     }     else if (a%2==0)     {         printf("Even");     } }

--- Even_or_Odd.c
+++ Even_or_Odd.c
@@ -1,14 +1,15 @@
+
 #include<stdio.h>
 int main()
 {
-  int a;
-  scanf("%d",&a);
-  if(a%2==0)
-  {
-      printf("Even");
-  }
-  else
-  {
-      printf("Odd");
-  }
+    int a;
+    scanf("%d",&a);
+    if (a%2!=0)
+    {
+        printf("Odd");
+    }
+    else if (a%2==0)
+    {
+        printf("Even");
+    }
 }
