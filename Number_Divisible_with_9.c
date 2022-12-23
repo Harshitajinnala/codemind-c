b62394eeb346ac2856d9242112fb98a5109c903f
Harshita Jinnala <119839384+Harshitajinnala@users.noreply.github.com>
2022-12-23 21:36:22 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     if (a%9==0)     {         printf("True");     }     else if (a%9!=0)     {         printf("False");     } }

--- Number_Divisible_with_9.c
+++ Number_Divisible_with_9.c
@@ -3,12 +3,13 @@ int main()
 {
     int a;
     scanf("%d",&a);
-    if(a%9==0)
+    if (a%9==0)
     {
         printf("True");
     }
-    else
+    else if (a%9!=0)
     {
         printf("False");
     }
 }
+
