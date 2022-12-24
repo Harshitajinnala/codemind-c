4221f2348e92bf79f889d7b38b3c835174bb4a73
Harshita Jinnala <119839384+Harshitajinnala@users.noreply.github.com>
2022-12-24 22:08:08 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(a<=b*1+(c*2))     {         printf("Qualify");     }     else     {         printf("Not Qualify");     } }

--- Contest:.c
+++ Contest:.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int x,a,b;
-    scanf("%d%d%d",&x,&a,&b);
-    if(x<=a+2*b)
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a<=b*1+(c*2))
     {
         printf("Qualify");
     }
