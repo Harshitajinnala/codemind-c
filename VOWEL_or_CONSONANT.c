e319907aa005bde646478b0c2f82927cb8498bb2
Harshita Jinnala <119839384+Harshitajinnala@users.noreply.github.com>
2022-12-24 22:09:55 +0530

    #include<stdio.h> int main() {     char c;     scanf("%c",&c);     if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u')     {         printf("VOWEL");     }     else if (c!='a' || c=='e' || c=='i' || c=='o' || c=='u')     {         printf("CONSONANT");     } }

--- VOWEL_or_CONSONANT.c
+++ VOWEL_or_CONSONANT.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    char ch;
-    scanf("%c",&ch);
-    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+    char c;
+    scanf("%c",&c);
+    if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
     {
         printf("VOWEL");
-        
     }
-    else
+    else if (c!='a' || c=='e' || c=='i' || c=='o' || c=='u')
     {
         printf("CONSONANT");
     }
 }
+
+
