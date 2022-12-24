0ded720cde1aa92ee71c25dc6b73bcb13ee7680c
Harshita Jinnala <119839384+Harshitajinnala@users.noreply.github.com>
2022-12-24 22:09:14 +0530

    #include<stdio.h> int main() {     int a,b,c,d,e;     scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);     if (a>=34 && b>=34 && c>=34 && d>=34 && e>=34)     {         printf("PASSED");     }     else     {         printf("FAILED");     }     return 0; }

--- Passed_or_Failed.c
+++ Passed_or_Failed.c
@@ -1,16 +1,16 @@
+
 #include<stdio.h>
 int main()
 {
     int a,b,c,d,e;
     scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
-    if(a>34&b>34&c>34&d>34&e>34)
+    if (a>=34 && b>=34 && c>=34 && d>=34 && e>=34)
     {
         printf("PASSED");
-        
     }
-    
     else
     {
         printf("FAILED");
     }
+    return 0;
 }
