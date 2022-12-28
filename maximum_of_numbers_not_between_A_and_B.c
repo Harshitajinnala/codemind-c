0411c9bd5d7688eff557aab08beb9079d12bb8cc
Harshita Jinnala <119839384+Harshitajinnala@users.noreply.github.com>
2022-12-28 20:56:28 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,m;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int b,c;     scanf("%d%d",&b,&c);     m=a[0];     for(i=1;i<n;i++)     {         if(m<a[i] && !(a[i]>=b && a[i]<=c))         {             m=a[i];         }     }     if(m==a[0])     printf("-1");     else     printf("%d",m); }

---
+++ maximum_of_numbers_not_between_A_and_B.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,m;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int b,c;
+    scanf("%d%d",&b,&c);
+    m=a[0];
+    for(i=1;i<n;i++)
+    {
+        if(m<a[i] && !(a[i]>=b && a[i]<=c))
+        {
+            m=a[i];
+        }
+    }
+    if(m==a[0])
+    printf("-1");
+    else
+    printf("%d",m);
+}
+
+
