46256eceac7b58d66861be002adab14771dba35d
Harshita Jinnala <119839384+Harshitajinnala@users.noreply.github.com>
2023-01-10 21:40:54 +0530

    #include<stdio.h> int main() {     int a,b,c,rev1=0,rev2=0,rem1,rem2,temp1,temp2;     scanf("%d",&a);     temp1=a*a;     while(a>0)     {         rem1=a%10;         rev1=rev1*10+rem1;         a=a/10;     }     b=rev1*rev1;     while(b>0)     {         rem2=b%10;         rev2=rev2*10+rem2;         b=b/10;     }     if(temp1==rev2)     printf("True");     else     printf("False"); }

--- Adam_number.c
+++ Adam_number.c
@@ -1,24 +1,24 @@
 #include<stdio.h>
 int main()
 {
-	int n,a,b,rev=0,r,rev1=0,r1;
-	scanf("%d",&n);
-	a=n*n;
-	while(n>0)
-	{
-	    r=n%10;
-	    rev=rev*10+r;
-	    n=n/10;
-	}
-	b=rev*rev;
-	while(b>0)
-	{
-	    r1=b%10;
-	    rev1=rev1*10+r1;
-	    b=b/10;
-	}
-	if(a==rev1)
-	printf("True");
-	else
-	printf("False");
+    int a,b,c,rev1=0,rev2=0,rem1,rem2,temp1,temp2;
+    scanf("%d",&a);
+    temp1=a*a;
+    while(a>0)
+    {
+        rem1=a%10;
+        rev1=rev1*10+rem1;
+        a=a/10;
+    }
+    b=rev1*rev1;
+    while(b>0)
+    {
+        rem2=b%10;
+        rev2=rev2*10+rem2;
+        b=b/10;
+    }
+    if(temp1==rev2)
+    printf("True");
+    else
+    printf("False");
 }
